5cd6b0c8561bca6f74f96855ce6ccddb040a0590
Mattia Maldini <mattia512maldini@gmail.com>
2020-04-01 09:14:33 +0200

    Field test corrections

--- generic/pagemanager/page_manager.h
+++ generic/pagemanager/page_manager.h
@@ -9,14 +9,21 @@ typedef struct {
     int              id;
     pman_page_data_t data;
     void *           extra;
+    int              popup;
 
+    // Called when the page is first created; it initializes and returns the data structures used by the page
     pman_page_data_t (*create_page)(pman_model_t model, unsigned long timestamp, void *extra);
+    // Called when the page definitively exits the scenes; should free all used memory
     void (*destroy_page)(pman_page_data_t data, void *extra);
 
+    // Called when the page enters view; it should setup all the required widgets
     void (*open_page)(pman_model_t model, pman_page_data_t data, unsigned long timestamp);
+    // Called when the page exits view
     void (*close_page)(pman_page_data_t data, unsigned long timestamp);
 
+    // Called to update the page's content
     void (*update_page)(pman_model_t model, pman_page_data_t data);
+    // Called to process an event
     pman_update_t (*process_event)(pman_model_t model, pman_page_data_t data, pman_event_t event,
                                    unsigned long timestamp);
 } pman_page_t;
@@ -24,11 +31,12 @@ typedef struct {
 STACK_DECLARATION(navigation_stack, pman_page_t, PMAN_NAVIGATION_DEPTH);
 
 typedef struct {
+    int                     initialized;
     pman_page_t             current_page;
     struct navigation_stack navq;
 } page_manager_t;
 
-void init_page_manager(page_manager_t *pman, pman_page_t page);
+void init_page_manager(page_manager_t *pman);
 
 void pman_back(page_manager_t *pman, pman_model_t model, unsigned long timestamp);
 void pman_change_page(page_manager_t *pman, pman_model_t model, pman_page_t page, unsigned long timestamp);
