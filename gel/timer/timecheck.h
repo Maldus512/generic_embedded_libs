456e521524e9f4befdf532a75681854a5127c820
Mattia Maldini <mattia512maldini@gmail.com>
2020-07-24 11:51:09 +0200

    Differentiated strict and loose time expiration

--- gel/timer/timecheck.h
+++ gel/timer/timecheck.h
@@ -1,22 +1,5 @@
-/******************************************************************************/
-/*                                                                            */
-/*  HSW snc - Casalecchio di Reno (BO) ITALY                                  */
-/*  ----------------------------------------                                  */
-/*                                                                            */
-/*  modulo: timecheck.h                                                       */
-/*                                                                            */
-/*      gestione check timer                                                  */
-/*                                                                            */
-/*  Autore: Maldus (Mattia MALDINI) & Massimo ZANNA                           */
-/*                                                                            */
-/*  Data  : 31/01/2020      REV  : 00.0                                       */
-/*                                                                            */
-/*  U.mod.: 04/04/2020      REV  : 01.0                                       */
-/*                                                                            */
-/******************************************************************************/
-
-#ifndef __TIMECHECK_H__
-#define __TIMECHECK_H__
+#ifndef TIMECHECK_H_INCLUDED
+#define TIMECHECK_H_INCLUDED
 
 #include <stdint.h>
 
@@ -30,20 +13,32 @@ typedef unsigned long timestamp_t;
         1;                                                                                                             \
     })
 
-#define time_after(a, b)  (typecheck(unsigned long, a) && typecheck(unsigned long, b) && ((long)((b) - (a)) < 0))
+#define time_after(a, b) (typecheck(unsigned long, a) && typecheck(unsigned long, b) && ((long)((b) - (a)) < 0))
+#define time_after_or_equal(a, b)                                                                                      \
+    (typecheck(unsigned long, a) && typecheck(unsigned long, b) && ((long)((b) - (a)) <= 0))
 #define time_before(a, b) time_after(b, a)
 
-static inline __attribute__((always_inline)) unsigned long time_interval(unsigned long a, unsigned long b)
-{
+#define is_expired(start, current, delay) is_loosely_expired(start, current, delay)
+
+
+static inline __attribute__((always_inline)) unsigned long time_interval(unsigned long a, unsigned long b) {
     if (time_after(a, b))
         return -((unsigned long)b - (unsigned long)a);
     else
         return (unsigned long)b - (unsigned long)a;
 }
 
-static inline __attribute__((always_inline)) int is_expired(unsigned long start, unsigned long current, unsigned long delay)
-{
+
+static inline __attribute__((always_inline)) int is_loosely_expired(unsigned long start, unsigned long current,
+                                                                    unsigned long delay) {
+    return time_after_or_equal(current, start + delay);
+}
+
+
+static inline __attribute__((always_inline)) int is_strictly_expired(unsigned long start, unsigned long current,
+                                                                     unsigned long delay) {
     return time_after(current, start + delay);
 }
 
-#endif      /* __TIMECHECK_H__ */ 
+
+#endif /* __TIMECHECK_H__ */
