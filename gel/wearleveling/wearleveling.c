efa372a6ab8f652a8d78a11c15ac9467316f582a
Mattia Maldini <mattia512maldini@gmail.com>
2021-06-18 08:15:36 +0200

    Refactored wear leveling module

--- gel/wearleveling/wearleveling.c
+++ gel/wearleveling/wearleveling.c
@@ -1,119 +1,131 @@
-#include <string.h>
+/**
+ * @file wearleveling.c
+ * @author Maldus512
+ * @brief Wear leveling abstraction module. It requires access to an external storage device organized in blocks, each
+ * indexed in order and with an associated byte marker (how the marker is associate is not a concern of this library).
+ * The access is gained through three function pointers:
+ *  - read_block: reads some bytes from a specified block
+ *  - read_marker: read just the marker from a specified block
+ *  - write_block: writes data (and the marker) to a specified block
+ *
+ * The idea is to abstract a storage section as big as a block that can be read and written normally but in reality is
+ * moved to a different block when changed. The marker for each block is incremented every time, so the last block that
+ * is not followed by a consequent marker is the last one that was active.
+ *
+ */
 
+#include <string.h>
 #include "wearleveling.h"
 
 
-/*
-  search for last used block.
-  return code:
-    negative: error
-    positive (or 0): index of last
-      i = wlm->blocks_in_page means some kind of error, but it should never happen
+#define NEXT_BLOCK(x, mod) ((x + 1) % mod)
 
-    every block can be described by (index, marker)
-    every block (i1,m1) is followed by another block (i2,m2) such that:
-    (i2 == MOD_NEXT(i1,blocks_in_page)) <==> (m2 == MOD_NEXT(m2,256)) XOR i2 is last used
-    this means that the only block which is not followed by a block marked by its next in module is the last used.
-    this stands true also for the last block of the list, hence the MOD_NEXT on the indexes (left side of the
-  implication before)
 
-    initialization:
-      during the first round the property will be true only for the blocks already used, and will always fail on the
-  first new block. since blocks are listed iteratively, they will be used in such order.
+static int search_last(wear_leveled_memory_t *wlm);
 
-    At most 255 blocks can be marked (256-1)(need one to ensure the property)
-    256 is hardcoded, it can be increased to 65536 using 2 bytes, but this is not considered a priority.
 
-    read can detect if the block is valid by checking the prevoius one
-*/
-int search_last(wear_leveled_memory_t *wlm) {
-    uint8_t tmp_buffer[WL_BLOCK_SIZE];
-    int32_t err;
-    size_t  last_idx;
-    size_t  i;
+/**
+ * @brief Read data from the wear leveled memory
+ *
+ * @param wlm Pointer to the wear leveled memory struct
+ * @param buffer Buffer where the data will be stored
+ * @param len Number of bytes to read
+ * @return int 0 if everything was ok, something else otherwise
+ */
+int wearleveling_read(wear_leveled_memory_t *wlm, uint8_t *buffer, size_t len) {
+    return wlm->read_block(wlm->last_active_block, buffer, len);
+}
 
-    // start from 0, and move right
-    if ((err = wlm->read_block(0, tmp_buffer))) {
+
+/**
+ * @brief Write data to the wear leveled memory
+ *
+ * @param wlm Pointer to the wear leveled memory struct
+ * @param buffer Buffer containing the data to be written
+ * @param len Number of bytes to write
+ * @return int 0 if everything was ok, something else otherwise
+ */
+int wearleveling_write(wear_leveled_memory_t *wlm, uint8_t *buffer, size_t len) {
+    uint8_t intermediate_buffer[len];
+    uint8_t last_marker;
+    int     res;
+
+    if ((res = wlm->read_marker(wlm->last_active_block, &last_marker))) {
+        return res;
+    }
+
+    if ((res = wlm->read_block(wlm->last_active_block, intermediate_buffer, len))) {
+        return res;
+    }
+
+    if (memcmp(intermediate_buffer, buffer, len)) {
+        uint8_t next_marker    = NEXT_BLOCK(last_marker, MAX_BLOCKS_IN_PAGE);
+        wlm->last_active_block = NEXT_BLOCK(wlm->last_active_block, wlm->blocks_in_page);
+
+        return wlm->write_block(wlm->last_active_block, next_marker, buffer, len);
+    }
+    return 0;
+}
+
+
+/**
+ * @brief Initialize the wear leveled memory data structure
+ *
+ * @param wlm Pointer to the structure to initialize
+ * @param read_block Function to read from a specific block of memory
+ * @param write_block Function to write a specific block of memory (and marker)
+ * @param read_marker Function to read the marker of a block
+ * @param blocks_in_page Number of blocks in page
+ */
+void wearleveling_init(wear_leveled_memory_t *wlm, wl_read_block_t read_block, wl_write_block_t write_block,
+             wl_read_marker_t read_marker, size_t blocks_in_page) {
+    assert(blocks_in_page < MAX_BLOCKS_IN_PAGE);
+    wlm->read_block     = read_block;
+    wlm->write_block    = write_block;
+    wlm->read_marker    = read_marker;
+    wlm->blocks_in_page = blocks_in_page;
+
+    wlm->last_active_block = search_last(wlm);
+}
+
+
+/**
+ * @brief Search for the last used block
+ *
+ * @param wlm Wear leveled memory data structure
+ * @return int 0 if everything was ok, something else otherwise
+ */
+static int search_last(wear_leveled_memory_t *wlm) {
+    uint8_t marker, last_marker;
+    int     err;
+
+    // start from 0 and move on
+    if ((err = wlm->read_marker(0, &last_marker))) {
         return err;
     }
-    last_idx = tmp_buffer[0];
 
+    size_t i;
     for (i = 1; i < wlm->blocks_in_page; i++) {
-        if ((err = wlm->read_block(i, tmp_buffer))) {
+        // Read the next marker
+        if ((err = wlm->read_marker(i, &marker))) {
             return err;
         }
 
-        if (tmp_buffer[0] != MOD_NEXT(last_idx, 256)) {
-            last_idx = tmp_buffer[0];
+        // If the current marker is not the one that should follow from the previous one (i.e. last_marker + 1 with mod
+        // limit) then we found the last active one
+        if (marker != NEXT_BLOCK(last_marker, MAX_BLOCKS_IN_PAGE)) {
             break;
+        } else {
+            // Otherwise keep looking
+            last_marker = marker;
         }
-        last_idx = tmp_buffer[0];
     }
+
+    if (i == wlm->blocks_in_page) {
+        // Something very wrong happened, fail
+        return -1;
+    }
+
+    // Return the index of the last active marker
     return i - 1;
-}
-
-
-int wl_read(wear_leveled_memory_t *wlm, uint8_t *buffer) {
-    uint8_t tmp_buffer[WL_BLOCK_SIZE];
-    int32_t err;
-    int     last_idx = search_last(wlm);
-    if (last_idx < 0) {
-        return last_idx;
-    }
-    if (last_idx == (int)wlm->blocks_in_page) {
-        return -1;
-        // what to do in a case that should never happen?
-        // it may mean the memory got overwritten or corrupted
-    }
-
-
-    if ((err = wlm->read_block(last_idx, tmp_buffer))) {
-        return err;
-    }
-    memcpy(buffer, tmp_buffer + 1, wlm->bytes_used_in_block);
-    return 0;
-}
-
-
-int wl_write(wear_leveled_memory_t *wlm, uint8_t *buffer) {
-
-    uint8_t tmp_buffer[WL_BLOCK_SIZE];
-    int32_t err;
-    int     last_idx = search_last(wlm);
-    size_t  last_mark;
-    if (last_idx < 0) {
-        return last_idx;
-    }
-    if (last_idx == (int)wlm->blocks_in_page) {
-        return -1;
-        // what to do in a case that should never happen?
-        // it may mean the memory got overwritten or corrupted
-    }
-
-    if ((err = wlm->read_block(last_idx, tmp_buffer))) {
-        return err;
-    }
-
-    last_mark = tmp_buffer[0];
-    // print("last mark found %d",last_mark);
-    if (memcmp(tmp_buffer + 1, buffer, wlm->bytes_used_in_block)) {
-        tmp_buffer[0] = 1;
-        memcpy(tmp_buffer + 1, buffer, wlm->bytes_used_in_block);
-        tmp_buffer[0] = MOD_NEXT(last_mark, 256);
-        if ((err = wlm->write_block(MOD_NEXT(last_idx, wlm->blocks_in_page), tmp_buffer))) {
-            return err;
-        }
-    }
-    return 0;
-}
-
-
-void wl_init(wear_leveled_memory_t *wlm, memory_cb_t write_block, memory_cb_t read_block, size_t blocks_in_page,
-             size_t bytes_used_in_block) {
-    assert(bytes_used_in_block < WL_BLOCK_SIZE);
-    assert(blocks_in_page < 256);
-    wlm->write_block         = write_block;
-    wlm->read_block          = read_block;
-    wlm->blocks_in_page      = blocks_in_page;
-    wlm->bytes_used_in_block = bytes_used_in_block;
 }
