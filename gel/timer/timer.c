d73b48b9f9c7d72d8829c48d9c1342985cec52d4
Mattia Maldini <mattia512maldini@gmail.com>
2022-01-26 14:57:46 +0100

    Changed conflicting namespace

--- gel/timer/timer.c
+++ gel/timer/timer.c
@@ -1,17 +1,8 @@
-/*typedef struct {
-    int active;
-    int autoreload;
-    stopwatch_t stopwatch;
-    timer_callback_t callback;
-    void *arg;
-} timer_t;*/
-
 #include "timer.h"
 #include "timecheck.h"
 
 
-
-timer_t *timer_activate(timer_t *timer, unsigned long period, unsigned long timestamp, timer_callback_t cb, void *arg) {
+gel_timer_t *gel_timer_activate(gel_timer_t *timer, unsigned long period, unsigned long timestamp, gel_timer_callback_t cb, void *arg) {
     stopwatch_init(&timer->stopwatch);
     timer->active     = 1;
     timer->callback   = cb;
@@ -22,28 +13,28 @@ timer_t *timer_activate(timer_t *timer, unsigned long period, unsigned long time
 }
 
 
-void timer_deactivate(timer_t *timer) {
+void gel_timer_deactivate(gel_timer_t *timer) {
     timer->active = 0;
     stopwatch_stop(&timer->stopwatch);
 }
 
 
-void timer_resume(timer_t *timer, unsigned long timestamp) {
+void gel_timer_resume(gel_timer_t *timer, unsigned long timestamp) {
     stopwatch_start(&timer->stopwatch, timestamp);
 }
 
 
-void timer_set_autoreload(timer_t *timer, int autoreload) {
+void gel_timer_set_autoreload(gel_timer_t *timer, int autoreload) {
     timer->autoreload = autoreload;
 }
 
 
-void timer_pause(timer_t *timer, unsigned long timestamp) {
+void gel_timer_pause(gel_timer_t *timer, unsigned long timestamp) {
     stopwatch_pause(&timer->stopwatch, timestamp);
 }
 
 
-int timer_manage_callbacks(timer_t *timers, size_t num, unsigned long timestamp, void *user_pointer) {
+int gel_timer_manage_callbacks(gel_timer_t *timers, size_t num, unsigned long timestamp, void *user_pointer) {
     int res = 0;
     for (size_t i = 0; i < num; i++) {
         if (stopwatch_is_timer_reached(&timers[i].stopwatch, timestamp)) {
@@ -61,7 +52,7 @@ int timer_manage_callbacks(timer_t *timers, size_t num, unsigned long timestamp,
 }
 
 
-int timer_get_first_available(timer_t *timers, size_t num) {
+int gel_timer_get_first_available(gel_timer_t *timers, size_t num) {
     for (size_t i = 0; i < num; i++) {
         if (timers[i].active == 0) {
             return i;
@@ -71,31 +62,31 @@ int timer_get_first_available(timer_t *timers, size_t num) {
 }
 
 
-unsigned long timer_get_remaining(timer_t *timer, unsigned long timestamp) {
+unsigned long gel_timer_get_remaining(gel_timer_t *timer, unsigned long timestamp) {
     return stopwatch_get_remaining(&timer->stopwatch, timestamp);
 }
 
 
-void timer_deactivate_all(timer_t *timers, size_t num) {
+void gel_timer_deactivate_all(gel_timer_t *timers, size_t num) {
     for (size_t i = 0; i < num; i++) {
-        timer_deactivate(&timers[i]);
+        gel_timer_deactivate(&timers[i]);
     }
 }
 
 
-void timer_pause_all(timer_t *timers, size_t num, unsigned long timestamp) {
+void gel_timer_pause_all(gel_timer_t *timers, size_t num, unsigned long timestamp) {
     for (size_t i = 0; i < num; i++) {
         if (timers[i].active) {
-            timer_pause(&timers[i], timestamp);
+            gel_timer_pause(&timers[i], timestamp);
         }
     }
 }
 
 
-void timer_resume_all(timer_t *timers, size_t num, unsigned long timestamp) {
+void gel_timer_resume_all(gel_timer_t *timers, size_t num, unsigned long timestamp) {
     for (size_t i = 0; i < num; i++) {
         if (timers[i].active) {
-            timer_resume(&timers[i], timestamp);
+            gel_timer_resume(&timers[i], timestamp);
         }
     }
 }
--- gel/timer/timer.h
+++ gel/timer/timer.h
@@ -5,33 +5,33 @@
 #include "stopwatch.h"
 
 
-#define TIMER_NULL ((timer_t){.active = 0, .autoreload = 0, .stopwatch = STOPWATCH_NULL, .callback = NULL, .arg = NULL})
+#define GEL_TIMER_NULL ((gel_timer_t){.active = 0, .autoreload = 0, .stopwatch = STOPWATCH_NULL, .callback = NULL, .arg = NULL})
 
 
 
 
-typedef struct timer_struct {
+typedef struct gel_timer_struct {
     int         active;
     int         autoreload;
     stopwatch_t stopwatch;
-    void (*callback)(struct timer_struct *, void *, void *);
+    void (*callback)(struct gel_timer_struct *, void *, void *);
     void *arg;
-} timer_t;
+} gel_timer_t;
 
-typedef void (*timer_callback_t)(timer_t *, void *, void *);
-
-
+typedef void (*gel_timer_callback_t)(gel_timer_t *, void *, void *);
 
 
-timer_t *timer_activate(timer_t *timer, unsigned long period, unsigned long millis, timer_callback_t cb, void *arg);
-void     timer_deactivate(timer_t *timer);
-void     timer_set_autoreload(timer_t *timer, int autoreload);
-int      timer_manage_callbacks(timer_t *timers, size_t num, unsigned long timestamp, void *user_pointer);
-int      timer_get_first_available(timer_t *timers, size_t num);
-void     timer_deactivate_all(timer_t *timers, size_t num);
-void     timer_pause(timer_t *timer, unsigned long timestamp);
-void     timer_pause_all(timer_t *timers, size_t num, unsigned long timestamp);
-void     timer_resume(timer_t *timer, unsigned long timestamp);
-void     timer_resume_all(timer_t *timers, size_t num, unsigned long timestamp);
+
+
+gel_timer_t *gel_timer_activate(gel_timer_t *timer, unsigned long period, unsigned long millis, gel_timer_callback_t cb, void *arg);
+void     gel_timer_deactivate(gel_timer_t *timer);
+void     gel_timer_set_autoreload(gel_timer_t *timer, int autoreload);
+int      gel_timer_manage_callbacks(gel_timer_t *timers, size_t num, unsigned long timestamp, void *user_pointer);
+int      gel_timer_get_first_available(gel_timer_t *timers, size_t num);
+void     gel_timer_deactivate_all(gel_timer_t *timers, size_t num);
+void     gel_timer_pause(gel_timer_t *timer, unsigned long timestamp);
+void     gel_timer_pause_all(gel_timer_t *timers, size_t num, unsigned long timestamp);
+void     gel_timer_resume(gel_timer_t *timer, unsigned long timestamp);
+void     gel_timer_resume_all(gel_timer_t *timers, size_t num, unsigned long timestamp);
 
 #endif
